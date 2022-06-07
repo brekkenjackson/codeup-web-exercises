79b967ca8d3e0189ae4ebbde1d08cce735e75d59
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-06-07 10:45:48 -0500

    Finished with loops exercises, beginning arrays.

---
+++ js/while.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+(function () {
+    function timesTwo () {
+        let num = 1;
+        while (num < 65536) {
+            num *= 2;
+            console.log(num);
+        }
+    }
+timesTwo();
+
+
+function iceCreamTruckCompany() {
+    let totalCones = Math.floor(Math.random() * 50) + 50;
+    let numberOfCustomers = 0
+
+    do {
+        numberOfCustomers++;
+        let customerPurchase = Math.floor(Math.random() * 5) + 1;
+        console.log("Customer buys " + customerPurchase + " cones. We have " + totalCones + " left.");
+        if (customerPurchase > totalCones) {
+            console.log("We do not have enough cones for that, sorry!");
+        } else {
+            totalCones -= customerPurchase;
+            console.log("Coming right up! We still have " + totalCones + " cones left!");
+        }
+    } while (totalCones > 0);
+
+    console.log("Holy cow! We sold every cone and it only took us " + numberOfCustomers + " customers!");
+}
+
+iceCreamTruckCompany();
+
+
+})();
