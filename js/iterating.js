4d912877f4a7f840388634aab53b88b32d0bd0d3
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-06-07 11:22:16 -0500

    Completed arrays exercise.

--- js/iterating.js
+++ js/iterating.js
@@ -7,11 +7,15 @@
      * 'names'.
      */
 
+    let names = ['Rob', 'Kenneth', 'Billy', 'Sam']
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
+    console.log(names.length);
 
     /**
      * TODO:
@@ -19,21 +23,34 @@
      * accessing each element's index.
      */
 
+    console.log("The name you're looking for is " + names[0]);
+    console.log("The name you're looking for is " + names[1]);
+    console.log("The name you're looking for is " + names[2]);
+    console.log("The name you're looking for is " + names[3]);
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for (let i = 0; i < names.length; i++) {
+        console.log('The name at index is ' + i + ' is ' + names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
 
+    names.forEach(function(name){
+        console.log("Here's a good name for you: " + name + ".");
+    })
+
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
-     * return an an element from it
+     * return an element from it
      * - first: returns the first item in the array
      * - second: returns the second item in the array
      * - last: returns the last item in the array
@@ -43,5 +60,23 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+let fastFoods = ["Taco Bell", "McDonald's", "Freddy's", "Wendy's", "ChickFilA"]
+
+    function firstItem() {
+        return fastFoods[0];
+    }
+    console.log(firstItem());
+
+
+    function secondItem() {
+        return fastFoods[1];
+    }
+    console.log(secondItem());
+
+
+    function fifthItem() {
+        return fastFoods[4];
+    }
+    console.log(fifthItem());
 
 })();
